302ad2f976aa32571a820f4ab9416955e7bebc2c
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2023-10-04 14:58:45 +0000

    salvando_no_arquivoBinario

--- banco.h
+++ banco.h
@@ -4,7 +4,7 @@ typedef struct{
 }operacao;
 
 typedef struct{
-  operacao extrato[100];
+  operacao extrato[100]; //informações das ultimas 100 operações
   char cpf[11];//número grande
   float saldo;
   int tipoConta;
@@ -18,8 +18,10 @@ typedef struct{
 }listaClientes;
 
 typedef enum{COMUM, PLUS} tipoConta; //comparação com enum é mais fácil  COMUM = 0, PLUS = 1
-typedef enum{err_cria,err_del,err_tipo,err_valor,err_saldo_comum}erros;
+typedef enum{err_cria,err_del,err_tipo,err_valor,err_saldo_comum}erros; //???????????????
+
 void menu();
+
 
 void criaCliente(listaClientes *Lc);
 void deletarCliente(listaClientes *Lc);
@@ -27,5 +29,8 @@ void listaCliente(listaClientes Lc);
 void debito(listaClientes *Lc);
 void deposito(listaClientes *Lc);
 void transferencia(listaClientes *Lc);
+int salva_clientes(listaClientes Lc, char nomeArquivo[]);
 
 void clearBuffer();
+
+//Tudo deve ser armazenado em arquivo binario, e o extrato tem que gerar um txt com todas as informações(data, tarifa,valor adicionado ou removido da conta, e saldo final)
