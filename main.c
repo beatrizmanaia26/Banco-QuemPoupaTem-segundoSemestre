59251269da5d900028d5523eea125d3cc7e479ed
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2023-10-17 20:40:41 +0000

    Salva_toda_hora

--- main.c
+++ main.c
@@ -35,7 +35,7 @@ int main(void) {
          transferencia(&Lc);
          break;
     }
-    
+    codigo = salva_clientes(Lc, arquivo);
   }while(opcao != 0);
 
   codigo = salva_clientes(Lc, arquivo);
